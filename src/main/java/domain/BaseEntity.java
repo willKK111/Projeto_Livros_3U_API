8f3a373d5320a9f2aafb9f718ba8e73d582f9112
willKK111 <willkk0298@gmail.com>
2025-07-14 17:22:10 -0300

    Classe BaseEntity finalizada

--- src/main/java/domain/BaseEntity.java
+++ src/main/java/domain/BaseEntity.java
@@ -1,13 +1,20 @@
 package domain;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 @RequiredArgsConstructor
+@Getter
+@Setter
 @Data
 @MappedSuperclass
 public abstract class BaseEntity {
@@ -15,4 +22,26 @@ public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    private LocalDateTime deletedAt = null;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt = null;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        BaseEntity that = (BaseEntity) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+
+
 }
